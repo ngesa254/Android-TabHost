609ccbaf1836d30ec8fa985841cc838c43929508
ngesa marvin <marvinngesa@gmail.com>
2018-02-05 04:12:37 +0300

    tabs initial commit

---
+++ app/src/main/java/io/droidmarvin/androidtabhost/TabHostActivity.java
@@ -0,0 +1,13 @@
+package io.droidmarvin.androidtabhost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_host);
+    }
+}
